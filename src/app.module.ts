7270aaf997b73e56f001169af6a8cf2699d49c65
rishithakkarv <rishi.thakkar@vertrical.com>
2022-02-23 23:57:40 +0530

    feat: connect db using typeorm mysql - create User table

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import entities from './typeorm';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'admin',
+      database: 'nest_demo_db',
+      entities,
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/typeorm/User.ts
@@ -0,0 +1,31 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn({
+    type: 'bigint',
+    name: 'user_id',
+  })
+  id: number;
+
+  @Column({
+    nullable: false,
+    default: '',
+  })
+  name: string;
+
+  @Column({
+    nullable: false,
+    default: '',
+  })
+  email: string;
+
+  @Column({
+    nullable: false,
+    default: '',
+  })
+  password: string;
+
+  @Column()
+  createdAt: Date;
+}
